89063a7d1102bc4166b17a43315cf2f8634d04da
Julian Katz <juliankatz@gmx.de>
2021-07-05 18:38:37 +0200

    display cards

--- lokal/script.js
+++ lokal/script.js
@@ -1,35 +1,78 @@
 "use strict";
-let addLinkForm = document.forms.namedItem("add-link-form");
-addLinkForm.addEventListener("submit", handleFormSubmit);
-let serverUrl = "http://localhost:8100/";
-// let serverUrl: string = "https://testgisjk.herokuapp.com/";
-getCards();
-// --- Admin ---
-// Handle
-async function handleFormSubmit(_event) {
-    _event.preventDefault();
-    let url = serverUrl;
-    let formData = new FormData(addLinkForm);
-    let query = new URLSearchParams(formData);
-    url = url + "?" + query.toString();
-    let response = await fetch(url);
-    let responseValue = await response.text();
-    console.log(responseValue);
-}
-async function getCards() {
-    try {
+var script;
+(function (script) {
+    let serverUrl = "http://localhost:8100/";
+    // let serverUrl: string = "https://testgisjk.herokuapp.com/";
+    // --- Admin Page ---
+    if (document.URL.match("admin")) {
+        let addLinkForm = document.forms.namedItem("add-link-form");
+        addLinkForm.addEventListener("submit", handleFormSubmit);
+        async function handleFormSubmit(_event) {
+            _event.preventDefault();
+            let url = serverUrl;
+            let formData = new FormData(addLinkForm);
+            let query = new URLSearchParams(formData);
+            url = url + "?" + query.toString();
+            let response = await fetch(url);
+            let responseValue = await response.text();
+            console.log(responseValue);
+        }
+        // Admin Cards Delete Btn
+        function deleteCards() {
+            let htmlCards = document.querySelectorAll(".card-container");
+        }
+        async function doAsync() {
+            let cardArea = document.getElementById("game-cards");
+            await displayCards(cardArea);
+            deleteCards();
+        }
+        doAsync();
+    }
+    // --- Game Page ---
+    if (document.URL.match("game")) {
+        async function doAsync() {
+            let cardArea = document.getElementById("game-cards");
+            await displayCards(cardArea);
+            let htmlCards = document.querySelectorAll(".card");
+            htmlCards.forEach(card => {
+                card.addEventListener("click", handleCardClick);
+            });
+        }
+        doAsync();
+    }
+    function handleCardClick(_event) {
+        let clickedCard = _event.currentTarget;
+        console.log(_event.currentTarget.attributes.id);
+    }
+    async function getCards() {
         let url = serverUrl;
         url = url + "getCards/";
         let response = await fetch(url);
         let cards = JSON.parse(await response.text());
         return cards;
     }
-    catch (error) {
-        console.log(error);
+    async function giveCardsID() {
+        let cardsIds1 = ["a0", "b0", "c0", "d0", "e0", "f0", "g0", "h0"];
+        let cards1 = await getCards();
+        let cardsIds = ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"];
+        let cards = await getCards();
+        for (const i in cards) {
+            cards[i].id = cardsIds[i];
+        }
+        for (const i in cards1) {
+            cards1[i].id = cardsIds1[i];
+        }
+        let cardsAndIds = cards.concat(cards1);
+        return cardsAndIds;
     }
-}
-async function log() {
-    console.log(await getCards());
-}
-log();
+    async function displayCards(_placeInside) {
+        let cardsAndIds = await giveCardsID();
+        for (const card of cardsAndIds) {
+            let htmlCard = document.createElement("div");
+            htmlCard.classList.add("card-container");
+            htmlCard.innerHTML = `<div class="card" id="${card.id}"><div class="card-back"></div><div class="card-front"><img src="${card.link}"alt="Memory Card"></div>`;
+            _placeInside.appendChild(htmlCard);
+        }
+    }
+})(script || (script = {}));
 //# sourceMappingURL=script.js.map
--- lokal/script.ts
+++ lokal/script.ts
@@ -1,42 +1,90 @@
-let addLinkForm: HTMLFormElement = document.forms.namedItem("add-link-form");
-addLinkForm.addEventListener("submit", handleFormSubmit);
-let serverUrl: string = "http://localhost:8100/";
-// let serverUrl: string = "https://testgisjk.herokuapp.com/";
+namespace script {
+    let serverUrl: string = "http://localhost:8100/";
+    // let serverUrl: string = "https://testgisjk.herokuapp.com/";
+    
+    
+    // --- Admin Page ---
+    if (document.URL.match("admin")) {
+        let addLinkForm: HTMLFormElement = document.forms.namedItem("add-link-form");
+        addLinkForm.addEventListener("submit", handleFormSubmit);
+        
+        async function handleFormSubmit(_event: Event): Promise<void> {
+            _event.preventDefault();
+            let url: string = serverUrl;
+            let formData: FormData = new FormData(addLinkForm);
+            let query: URLSearchParams = new URLSearchParams(<any>formData);
+            url = url + "?" + query.toString();
+            let response: Response = await fetch(url);
+            let responseValue: string = await response.text();
+            console.log(responseValue);
+        }
 
-getCards();
-
-// --- Admin ---
-// Handle
-async function handleFormSubmit(_event: Event): Promise<void> {
-    _event.preventDefault();
-    let url: string = serverUrl;
-    let formData: FormData = new FormData(addLinkForm);
-    let query: URLSearchParams = new URLSearchParams(<any>formData);
-    url = url + "?" + query.toString();
-    let response: Response = await fetch(url);
-    let responseValue: string = await response.text();
-    console.log(responseValue);
-}
+        // Admin Cards Delete Btn
+        function deleteCards(): void {
+            let htmlCards: NodeList = document.querySelectorAll(".card-container");
+        }
 
 
-// --- Cards ---
-interface Card {
-    id: string;
-    link: string;
-}
-async function getCards(): Promise<Card[]> {
-    try {
+        async function doAsync(): Promise<void> {
+            let cardArea: HTMLElement = document.getElementById("game-cards");
+            await displayCards(cardArea);
+            deleteCards();
+        }
+        doAsync();
+
+    }
+    // --- Game Page ---
+    if (document.URL.match("game")) {
+        async function doAsync(): Promise<void> {
+            let cardArea: HTMLElement = document.getElementById("game-cards");
+            await displayCards(cardArea);
+            let htmlCards: NodeList = document.querySelectorAll(".card");
+            htmlCards.forEach(card => {
+                card.addEventListener("click", handleCardClick);
+                
+            });
+        }
+        doAsync();
+        
+    }
+    function handleCardClick(_event: Event): void {
+        let clickedCard: HTMLElement = _event.currentTarget;
+        console.log(_event.currentTarget.attributes.id);
+    }
+    // --- Cards ---
+    interface Card {
+        id: string;
+        link: string;
+    }
+    async function getCards(): Promise<Card[]> {
         let url: string = serverUrl;
         url = url + "getCards/";
         let response: Response = await fetch(url);
         let cards: Card[] = JSON.parse(await response.text());
         return cards;
-    } catch (error) {
-        console.log(error);
+    }
+    async function giveCardsID(): Promise<Card[]> {
+        let cardsIds1: string[] = ["a0", "b0", "c0", "d0", "e0", "f0", "g0", "h0"];
+        let cards1: Card[] = await getCards();
+        let cardsIds: string[] = ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"];
+        let cards: Card[] = await getCards();
+        for (const i in cards) {
+            cards[i].id = cardsIds[i];  
+        }
+        for (const i in cards1) {
+            cards1[i].id = cardsIds1[i];  
+        }
+        let cardsAndIds: Card[] = cards.concat(cards1);
+        return cardsAndIds;
+    }
+    async function displayCards(_placeInside: HTMLElement): Promise<void> {
+        let cardsAndIds: Card[] = await giveCardsID();
+        for (const card of cardsAndIds ) {
+        let htmlCard: HTMLElement = document.createElement("div");
+        htmlCard.classList.add("card-container");
+        htmlCard.innerHTML = `<div class="card" id="${card.id}"><div class="card-back"></div><div class="card-front"><img src="${card.link}"alt="Memory Card"></div>`;
+        _placeInside.appendChild(htmlCard);
+            }
+
     }
 }
-async function log(): Promise<void> {
-    console.log( await getCards());
-}
-log();
-
