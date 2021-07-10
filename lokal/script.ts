84813939759430476229b886983fab8104c7cdfe
Julian Katz <juliankatz@gmx.de>
2021-07-10 18:03:20 +0200

    server to heroku

--- lokal/script.ts
+++ lokal/script.ts
@@ -1,6 +1,6 @@
 namespace script {
-    export let serverUrl: string = "http://localhost:8100/";
-    // export let serverUrl: string = "https://testgisjk.herokuapp.com/";
+    // export let serverUrl: string = "http://localhost:8100/";
+    export let serverUrl: string = "https://testgisjk.herokuapp.com/";
     
     
     // --- Admin Page ---
@@ -8,8 +8,8 @@ namespace script {
     if (document.URL.match("admin.html")) {
         async function doAsync(): Promise<void> {
             await displayCards(cardArea, true);
-            addBin();
             addEventListenerDeleteCards();
+            addBin();
         }
         doAsync();
         // Admin Form
@@ -146,7 +146,6 @@ namespace script {
         svg.innerHTML = `<path d="M268 416h24a12 12 0 0 0 12-12V188a12 12 0 0 0-12-12h-24a12 12 0 0 0-12 12v216a12 12 0 0 0 12 12zM432 80h-82.41l-34-56.7A48 48 0 0 0 274.41 0H173.59a48 48 0 0 0-41.16 23.3L98.41 80H16A16 16 0 0 0 0 96v16a16 16 0 0 0 16 16h16v336a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128h16a16 16 0 0 0 16-16V96a16 16 0 0 0-16-16zM171.84 50.91A6 6 0 0 1 177 48h94a6 6 0 0 1 5.15 2.91L293.61 80H154.39zM368 464H80V128h288zm-212-48h24a12 12 0 0 0 12-12V188a12 12 0 0 0-12-12h-24a12 12 0 0 0-12 12v216a12 12 0 0 0 12 12z"/>`;
         cardFronts.forEach((cardFront) => {
             cardFront.prepend(svg);
-            document.querySelector("body").appendChild(svg);
         });
     }
     // ---- Game Funktionen ----
